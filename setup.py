9d298b6d143d04e53d2fe60755b56e5a4932c590
Philip Fowler <philipwfowler@gmail.com>
2018-01-21 11:45:59 +0000

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     install_requires=[
         "numpy >= 1.13",
-        "json >= 2.09",
         "requests >= 2.8",
         "pandas >= 0.21.0",
         "tqdm >= 4.19"
